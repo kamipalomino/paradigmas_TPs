a9502390ffe99df87c9214ae8216278af3ae4ac9
LucasGaleano <l.a.galeano2@gmail.com>
2018-05-15 17:45:15 -0300

    arreglo eventos

--- blockChain.hs
+++ blockChain.hs
@@ -17,33 +17,33 @@ ejecutarTest = hspec $ do
 
   describe "Pruebas sobre los eventos" $ do
 
-    it "Depositar 10 más. Debería quedar con 20 monedas." $ (billetera.deposita 10) usuarioCon10Monedas `shouldBe` 20
-    it "Extraer 3: Debería quedar con 7" $ (billetera.extraccion 3) usuarioCon10Monedas `shouldBe` 7
-    it "Extraer 15: Debería quedar con 0" $ (billetera.extraccion 15) usuarioCon10Monedas `shouldBe` 0
-    it "Un upgrade: Debería quedar con 12." $  (billetera.upgrade) usuarioCon10Monedas `shouldBe` 12
-    it "Cerrar la cuenta: 0." $  (billetera.cierraCuenta) usuarioCon10Monedas `shouldBe` 0
-    it "Queda igual: 10." $ (billetera.quedaIgual) usuarioCon10Monedas  `shouldBe` 10
-    it "Depositar 1000, y luego tener un upgrade: 1020." $ (billetera.upgrade.deposita 1000) usuarioCon10Monedas `shouldBe` 1020
-    it "Toco y me voy, deberia quedar con 0." $ (billetera.tocoMeVoy) usuarioCon10Monedas `shouldBe` 0
-    it "ahorrante errante, deberia quedar con 34." $ (billetera.ahorranteErrante) usuarioCon10Monedas`shouldBe` 34
+    it "Depositar 10 más. Debería quedar con 20 monedas." $ deposita 10 usuarioCon10Monedas `shouldBe` 20
+    it "Extraer 3: Debería quedar con 7" $ extraccion 3 usuarioCon10Monedas `shouldBe` 7
+    it "Extraer 15: Debería quedar con 0" $ extraccion 15 usuarioCon10Monedas `shouldBe` 0
+    it "Un upgrade: Debería quedar con 12." $  upgrade usuarioCon10Monedas `shouldBe` 12
+    it "Cerrar la cuenta: 0." $  cierraCuenta usuarioCon10Monedas `shouldBe` 0
+    it "Queda igual: 10." $ quedaIgual usuarioCon10Monedas  `shouldBe` 10
+    it "Depositar 1000, y luego tener un upgrade: 1020." $ (upgrade.deposita 1000) usuarioCon10Monedas `shouldBe` 1020
+    it "Toco y me voy, deberia quedar con 0." $ tocoMeVoy usuarioCon10Monedas `shouldBe` 0
+    it "ahorrante errante, deberia quedar con 34." $ ahorranteErrante usuarioCon10Monedas`shouldBe` 34
 
   describe "Pruebas sobre los usuarios" $ do
-    it "Cual es la billetera de Pepe?, deberia ser 10 monedas" $ (billetera.quedaIgual) pepe `shouldBe` 10
-    it "Cual es la billetera de Pepe?, luego de un cierre de cuenta deberia ser 0 monedas" $ (billetera.cierraCuenta) pepe `shouldBe` 0
-    it "Como queda la biletera de pepe si le epositan 15, se extrae 2 y tiene un upgrade, deberia ser 27,6" $  (billetera.upgrade.extraccion 2.deposita 15) pepe `shouldBe` 27.6
+    it "Cual es la billetera de Pepe?, deberia ser 10 monedas" $ quedaIgual pepe `shouldBe` 10
+    it "Cual es la billetera de Pepe?, luego de un cierre de cuenta deberia ser 0 monedas" $ cierraCuenta pepe `shouldBe` 0
+    it "Como queda la biletera de pepe si le epositan 15, se extrae 2 y tiene un upgrade, deberia ser 27,6" $  (upgrade.extraccion 2.deposita 15) pepe `shouldBe` 27.6
 
   describe "Pruebas sobre las transiciones" $ do
 
-    it "Lucho toca y se va con billetera de 10 monedas, deberia quedar 0 monedas." $ (billetera.luchoTocaYSeVa lucho) usuarioCon10Monedas `shouldBe` 0
-    it "Lucho es ahorrante errante con billetera de 10 monedas, deberia quedar 34 monedas." $ (billetera.luchoEsUnAhorranteErrante lucho) usuarioCon10Monedas `shouldBe` 34
-    it "pepe le paga 7 monedas a lucho, deberia quedarle 3 monedas" $ (billetera.pepeLeDa7UnidadesALucho pepe) usuarioCon10Monedas `shouldBe` 3
-    it "lucho recibe 7 monedas de pepe, deberia quedarle 17 monedas" $ (billetera.pepeLeDa7UnidadesALucho lucho) usuarioCon10Monedas `shouldBe` 17
-    it "lucho cierra la cuenta aplicada a pepe, deberia quedar pepe sin cambios" $ (billetera.luchoCierraLaCuenta pepe) pepe `shouldBe` billetera pepe -- TODO Preguntar
-    it "pepe le da 7 unidades a Lucho, deberia quedar lucho con 9 monedas" $  (billetera.pepeLeDa7UnidadesALucho lucho) lucho `shouldBe` 9
-    it "pepe le da 7 unidades a Lucho y despues pepe deposita 5 monedas, deberia quedar con 8 monedas" $   (billetera.pepeLeDa7UnidadesALucho pepe.pepeDeposita5Monedas pepe) pepe `shouldBe` 8
-    it "Impactar la transacción 1 a Pepe. Debería quedar igual que como está inicialmente."$ (billetera.usuarioLuegoDeTransaccion luchoCierraLaCuenta) pepe `shouldBe` 10
-    it "Impactar la transacción 5 a Lucho. Debería producir que Lucho tenga 9 monedas en su billetera."$ (billetera.usuarioLuegoDeTransaccion pepeLeDa7UnidadesALucho) lucho `shouldBe` 9
-    it "Impactar la transacción 5 y luego la 2 a Pepe. Eso hace que tenga 8 en su billetera."$ (billetera.usuarioLuegoDeTransaccion pepeDeposita5Monedas.usuarioLuegoDeTransaccion pepeLeDa7UnidadesALucho) pepe `shouldBe` 8
+    it "Lucho toca y se va con billetera de 10 monedas, deberia quedar 0 monedas." $ luchoTocaYSeVa lucho usuarioCon10Monedas `shouldBe` 0
+    it "Lucho es ahorrante errante con billetera de 10 monedas, deberia quedar 34 monedas." $ luchoEsUnAhorranteErrante lucho usuarioCon10Monedas `shouldBe` 34
+    it "pepe le paga 7 monedas a lucho, deberia quedarle 3 monedas" $ pepeLeDa7UnidadesALucho pepe usuarioCon10Monedas `shouldBe` 3
+    it "lucho recibe 7 monedas de pepe, deberia quedarle 17 monedas" $ pepeLeDa7UnidadesALucho lucho usuarioCon10Monedas `shouldBe` 17
+    it "lucho cierra la cuenta aplicada a pepe, deberia quedar pepe sin cambios" $ luchoCierraLaCuenta pepe pepe `shouldBe` billetera pepe -- TODO Preguntar
+    it "pepe le da 7 unidades a Lucho, deberia quedar lucho con 9 monedas" $  pepeLeDa7UnidadesALucho lucho lucho `shouldBe` 9
+    it "pepe le da 7 unidades a Lucho y despues pepe deposita 5 monedas, deberia quedar con 8 monedas" $   (pepeLeDa7UnidadesALucho pepe.pepeDeposita5Monedas pepe) pepe `shouldBe` 8
+    it "Impactar la transacción 1 a Pepe. Debería quedar igual que como está inicialmente."$ usuarioLuegoDeTransaccion luchoCierraLaCuenta pepe `shouldBe` 10
+    it "Impactar la transacción 5 a Lucho. Debería producir que Lucho tenga 9 monedas en su billetera."$ usuarioLuegoDeTransaccion pepeLeDa7UnidadesALucho lucho `shouldBe` 9
+    it "Impactar la transacción 5 y luego la 2 a Pepe. Eso hace que tenga 8 en su billetera."$ (usuarioLuegoDeTransaccion pepeDeposita5Monedas.usuarioLuegoDeTransaccion pepeLeDa7UnidadesALucho) pepe `shouldBe` 8
 
   --describe "Pruebas sobre Bloques" $ do
 
@@ -54,7 +54,7 @@ ejecutarTest = hspec $ do
 
 -- NO SE ENTIENDE QUE FUNCION CUMPLE EL TYPE Transaccion SI ES IGUAL AL Evento Y DE TODAS MANERAS NO SE USA "Evento" EN EL CODIGO Y DEBERIA USARSE
 
-type Evento = Usuario -> Usuario
+type Evento = Usuario -> Monedas
 type Transaccion = Usuario -> Evento
 type Pago = Usuario -> Usuario -> Monedas -> Usuario -> Evento
 type Monedas = Float
@@ -73,17 +73,15 @@ lucho = Usuario "luciano" 2
 
 -----------EVENTOS-------------------------
 -- TIENEN QUE DELEGAR MAS EN LA FUNCION extraccion Y NO!! USEN GUARDA CUANDO DELEGUEN LA FUNCION (VIMOS UN EJEMPLO PARECIDO EN LAS PRIMERAS CLASES )
-quedaIgual :: Usuario -> Usuario
-quedaIgual usuario = id usuario -- USAR LA FUNCION id TODO preguntar id
-nuevaBilletera unMonto unUsuario = unUsuario {billetera = unMonto}
-cierraCuenta usuario = nuevaBilletera 0 usuario
-deposita unMonto usuario = nuevaBilletera ((billetera usuario) +  unMonto) usuario
-tocoMeVoy usuario = (cierraCuenta.upgrade.deposita 15) usuario
-ahorranteErrante usuario = (deposita 10 .upgrade .deposita 8 .extraccion 1 .deposita 2 .deposita 1) usuario
 
-extraccion unMonto usuario
-  | (billetera usuario) - unMonto < 0 = cierraCuenta usuario
-  | otherwise = nuevaBilletera ((billetera usuario) - unMonto) usuario
+quedaIgual :: Usuario -> Monedas
+quedaIgual = billetera  -- USAR LA FUNCION id TODO preguntar id
+cierraCuenta _ = 0
+deposita unMonto = unMonto + billetera
+tocoMeVoy unUsuario = (cierraCuenta.upgrade.deposita 15) unUsuario
+ahorranteErrante  = (deposita 10 .upgrade .deposita 8 .extraccion 1 .deposita 2 .deposita 1)
+
+extraccion unMonto unUsuario =  max(0, (billetera unUsuario) - unMonto )
 
 minCantidadMonedasUpgrade usuario = (billetera usuario + (min (billetera usuario * 1.2 - billetera usuario) 10))
 upgrade usuario = nuevaBilletera (minCantidadMonedasUpgrade usuario) usuario

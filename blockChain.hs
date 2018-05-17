3d6ec2a71e49959ecbe68cd0df9866365a0b5fe5
LucasGaleano <l.a.galeano2@gmail.com>
2018-05-16 23:47:05 -0300

    cambio Evento::Usuario->Monedas a Evento::Monedas->Monedas

--- blockChain.hs
+++ blockChain.hs
@@ -10,34 +10,34 @@ import Test.Hspec
 -- EN LOS TEST SE ESTA COMPARANDO USUARIOS CON UN NUMERO ( AL MARGEN DE QUE EL ENUNCIADO PEDIA DEVOLVER BILLETERAS Y DEVUELVEN USUARIOS ) Y NO SE PUEDE COMPARAR DOS TIPOS DIFERENTES
 -- TAMBIEN HAY MUCHOS PARENTESIS DE MAS, RELEER O REPASAR LA PRESEDENCIA DE LAS FUNCIONES EN HASKELL
 
-usuarioCon10Monedas = Usuario "Pepe" 10
+billeteraDeUsuarioCon10Monedas = billetera (Usuario "pepe" 10)
 
 
 ejecutarTest = hspec $ do
 
   describe "Pruebas sobre los eventos" $ do
 
-    it "Depositar 10 más. Debería quedar con 20 monedas." $ deposita 10 usuarioCon10Monedas `shouldBe` 20
-    it "Extraer 3: Debería quedar con 7" $ extraccion 3 usuarioCon10Monedas `shouldBe` 7
-    it "Extraer 15: Debería quedar con 0" $ extraccion 15 usuarioCon10Monedas `shouldBe` 0
-    it "Un upgrade: Debería quedar con 12." $  upgrade usuarioCon10Monedas `shouldBe` 12
-    it "Cerrar la cuenta: 0." $  cierraCuenta usuarioCon10Monedas `shouldBe` 0
-    it "Queda igual: 10." $ quedaIgual usuarioCon10Monedas  `shouldBe` 10
-    --it "Depositar 1000, y luego tener un upgrade: 1020." $ upgrade.deposita 1000 usuarioCon10Monedas `shouldBe` 1020
-    it "Toco y me voy, deberia quedar con 0." $ tocoMeVoy usuarioCon10Monedas `shouldBe` 0
-    --it "ahorrante errante, deberia quedar con 34." $ ahorranteErrante usuarioCon10Monedas`shouldBe` 34
+    it "Depositar 10 más. Debería quedar con 20 monedas." $ deposita 10 billeteraDeUsuarioCon10Monedas `shouldBe` 20
+    it "Extraer 3: Debería quedar con 7" $ extraccion 3 billeteraDeUsuarioCon10Monedas `shouldBe` 7
+    it "Extraer 15: Debería quedar con 0" $ extraccion 15 billeteraDeUsuarioCon10Monedas `shouldBe` 0
+    it "Un upgrade: Debería quedar con 12." $  upgrade billeteraDeUsuarioCon10Monedas `shouldBe` 12
+    it "Cerrar la cuenta: 0." $  cierraCuenta billeteraDeUsuarioCon10Monedas `shouldBe` 0
+    it "Queda igual: 10." $ quedaIgual billeteraDeUsuarioCon10Monedas  `shouldBe` 10
+    it "Depositar 1000, y luego tener un upgrade: 1020." $ (upgrade.deposita 1000) billeteraDeUsuarioCon10Monedas `shouldBe` 1020
+    it "Toco y me voy, deberia quedar con 0." $ tocoMeVoy billeteraDeUsuarioCon10Monedas `shouldBe` 0
+    it "ahorrante errante, deberia quedar con 34." $ ahorranteErrante billeteraDeUsuarioCon10Monedas`shouldBe` 34
 
   describe "Pruebas sobre los usuarios" $ do
-    it "Cual es la billetera de Pepe?, deberia ser 10 monedas" $ quedaIgual pepe `shouldBe` 10
-    it "Cual es la billetera de Pepe?, luego de un cierre de cuenta deberia ser 0 monedas" $ cierraCuenta pepe `shouldBe` 0
-    --it "Como queda la biletera de pepe si le epositan 15, se extrae 2 y tiene un upgrade, deberia ser 27,6" $  (upgrade.extraccion 2.deposita 15) pepe `shouldBe` 27.6
-
+    it "Cual es la billetera de Pepe?, deberia ser 10 monedas" $ (quedaIgual.billetera) pepe `shouldBe` 10
+    it "Cual es la billetera de Pepe?, luego de un cierre de cuenta deberia ser 0 monedas" $ (cierraCuenta. billetera) pepe `shouldBe` 0
+    it "Como queda la biletera de pepe si le epositan 15, se extrae 2 y tiene un upgrade, deberia ser 27,6" $  (upgrade.extraccion 2.deposita 15.billetera) pepe `shouldBe` 27.6
+{-}
   describe "Pruebas sobre las transiciones" $ do
 
-    it "Lucho toca y se va con billetera de 10 monedas, deberia quedar 0 monedas." $ luchoTocaYSeVa lucho usuarioCon10Monedas `shouldBe` 0
-    --it "Lucho es ahorrante errante con billetera de 10 monedas, deberia quedar 34 monedas." $ luchoEsUnAhorranteErrante lucho usuarioCon10Monedas `shouldBe` 34
-    it "pepe le paga 7 monedas a lucho, deberia quedarle 3 monedas" $ pepeLeDa7UnidadesALucho pepe usuarioCon10Monedas `shouldBe` 3
-    it "lucho recibe 7 monedas de pepe, deberia quedarle 17 monedas" $ pepeLeDa7UnidadesALucho lucho usuarioCon10Monedas `shouldBe` 17
+    it "Lucho toca y se va con billetera de 10 monedas, deberia quedar 0 monedas." $ luchoTocaYSeVa lucho billeteraDeUsuarioCon10Monedas `shouldBe` 0
+    --it "Lucho es ahorrante errante con billetera de 10 monedas, deberia quedar 34 monedas." $ luchoEsUnAhorranteErrante lucho billeteraDeUsuarioCon10Monedas `shouldBe` 34
+    it "pepe le paga 7 monedas a lucho, deberia quedarle 3 monedas" $ pepeLeDa7UnidadesALucho pepe billeteraDeUsuarioCon10Monedas `shouldBe` 3
+    it "lucho recibe 7 monedas de pepe, deberia quedarle 17 monedas" $ pepeLeDa7UnidadesALucho lucho billeteraDeUsuarioCon10Monedas `shouldBe` 17
     it "lucho cierra la cuenta aplicada a pepe, deberia quedar pepe sin cambios" $ luchoCierraLaCuenta pepe pepe `shouldBe` billetera pepe -- TODO Preguntar
     it "pepe le da 7 unidades a Lucho, deberia quedar lucho con 9 monedas" $  pepeLeDa7UnidadesALucho lucho lucho `shouldBe` 9
     --it "pepe le da 7 unidades a Lucho y despues pepe deposita 5 monedas, deberia quedar con 8 monedas" $   (pepeLeDa7UnidadesALucho pepe.pepeDeposita5Monedas pepe) pepe `shouldBe` 8
@@ -49,14 +49,14 @@ ejecutarTest = hspec $ do
 
 --    it "Aplicar bloque1 a pepe. Debería quedar con su mismo nombre, pero con una billetera de 18." $ (billetera.aplicarBloque bloque1) pepe `shouldBe` 18
     -- it "Probar que para el bloque 1, y los usuarios Pepe y Lucho, el único que quedaría con un saldo mayor a 10, es Pepe." $ lleganANCreditos bloque1 10 usuarios `shouldBe` [pepe]
-
+-}
 -----------TIPOS DE DATOS-----------------
 
 -- NO SE ENTIENDE QUE FUNCION CUMPLE EL TYPE Transaccion SI ES IGUAL AL Evento Y DE TODAS MANERAS NO SE USA "Evento" EN EL CODIGO Y DEBERIA USARSE
 
-type Evento = Usuario -> Monedas
+type Evento = Monedas -> Monedas
 type Transaccion = Usuario -> Evento
-type Pago = Transaccion
+type Pago = Usuario -> Usuario -> Monedas -> Usuario -> Evento
 type Monedas = Float
 type Nombre = String
 type Nivel = Int
@@ -81,19 +81,20 @@ cierraCuenta:: Evento
 upgrade :: Evento
 deposita::Monedas -> Evento
 extraccion::Monedas -> Evento
-tocoMeVoy::Evento
+--tocoMeVoy::Evento
 
-quedaIgual = billetera   -- USAR LA FUNCION id TODO preguntar id
-cierraCuenta  = billetera.nuevaBilletera 0
-deposita unMonto = ((+) unMonto .billetera)
+quedaIgual cantBilletera = id cantBilletera  -- USAR LA FUNCION id TODO preguntar id
+cierraCuenta _ = 0
+deposita unMonto = (+) unMonto
 
-tocoMeVoy unUsuario = (cierraCuenta.nuevaBilletera (upgrade unUsuario).nuevaBilletera (deposita 15 unUsuario))  unUsuario --TODO seguro esta mal esto
---ahorranteErrante unUsuario =  (unUsuario) id [deposita 10 .upgrade .deposita 8 .extraccion 1 .deposita 2 .deposita 1]
+tocoMeVoy = cierraCuenta.upgrade.deposita 15 --TODO seguro esta mal esto
+ahorranteErrante cantBilletera =  (deposita 10 .upgrade .deposita 8 .extraccion 1 .deposita 2 .deposita 1) cantBilletera
 
-extraccion unMonto  =  (max 0. (+) (-unMonto). billetera)
 
-minCantidadMonedasUpgrade unUsuario = (billetera unUsuario + (min (billetera unUsuario * 1.2 - billetera unUsuario) 10))
-upgrade unUsuario = (minCantidadMonedasUpgrade unUsuario) + billetera unUsuario
+extraccion unMonto = max 0. (+) (-unMonto)
+
+minCantidadMonedasUpgrade cantBilletera = (min (cantBilletera * 1.2 - cantBilletera) 10)
+upgrade cantBilletera = (minCantidadMonedasUpgrade cantBilletera) + cantBilletera
 
 
 ------------------TRANSACCIONES------------------
@@ -106,24 +107,24 @@ luchoCierraLaCuenta :: Transaccion
 luchoCierraLaCuenta = generarEventoAUnUsuario cierraCuenta lucho
 
 pepeDeposita5Monedas :: Transaccion
-pepeDeposita5Monedas  =  generarEventoAUnUsuario (deposita 5) lucho
+pepeDeposita5Monedas  =  generarEventoAUnUsuario (deposita 5) pepe
 
 luchoTocaYSeVa :: Transaccion
 luchoTocaYSeVa  =  generarEventoAUnUsuario tocoMeVoy lucho
 
 --luchoEsUnAhorranteErrante :: Transaccion
---luchoEsUnAhorranteErrante usuarioAVerificar = aplicarEvento lucho usuarioAVerificar ahorranteErrante
+--luchoEsUnAhorranteErrante usuarioAVerificar = generarEventoAUnUsuario ahorranteErrante lucho
 
 
 -----------------------PAGOS----------------------
 
-usuarioPagaAOtroUsuario :: Usuario -> Usuario -> Monedas -> Usuario -> Evento
+usuarioPagaAOtroUsuario :: Pago
 usuarioPagaAOtroUsuario usuarioQuePaga usuarioQueCobra cantidad unUsuario
   | compararUsuariosPorPorNombre usuarioQuePaga unUsuario   = extraccion cantidad
   | compararUsuariosPorPorNombre usuarioQueCobra unUsuario = deposita cantidad
   | otherwise = quedaIgual
 
-pepeLeDa7UnidadesALucho :: Pago
+pepeLeDa7UnidadesALucho :: Transaccion
 pepeLeDa7UnidadesALucho = usuarioPagaAOtroUsuario pepe lucho 7     --TODO preguntar pagos
 
 -------------FUNCIONES AUXILIARES-----------------
